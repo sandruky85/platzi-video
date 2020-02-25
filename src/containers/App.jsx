dd41a537f3a3e9b2f109c4b764e7b349a40b09d3
sandra.menendez <sandra.menendez@rakuten.de>
2020-02-25 22:21:05 +0100

    Initial config and first component

---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+import '../assets/styles/App.scss';
+
+const App = () => (
+  <div className='App'>
+    <Header />
+  </div>
+);
+
+export default App;
